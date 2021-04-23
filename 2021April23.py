f71a7320d6e85b12538f4809f4023ee0cd41f96b
Thomas Rochais <thb@sas.upenn.edu>
2021-04-23 12:49:18 -0400

    Flatten nested dictionary

---
+++ 2021April23.py
@@ -0,0 +1,17 @@
+import collections
+
+def flatten_dict(d, parent_key='', sep='.'):
+    return { parent_key + sep + k if parent_key else k : v
+             for kk, vv in d.items()
+             for k, v in flatten_dict(vv, kk, sep).items()
+            } if isinstance(d, dict) else {parent_key: d}
+
+print(flatten_dict({
+    "key": 3,
+    "foo": {
+        "a": 5,
+        "bar": {
+            "baz": 8
+        }
+    }
+}))
