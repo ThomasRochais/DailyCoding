01909a4a5f4c4ac8a46cdafec7c5c686520b70c1
Thomas Rochais <thb@sas.upenn.edu>
2021-05-10 17:28:11 -0400

    Rename files

--- Problem1.py
+++ Problem1.py
@@ -22,14 +22,9 @@ class Solution:
 N = 20
 nums_list = []
 k = []
-elapsed = []
 for i in range(N):
     k.append(random.randint(0,2000))
     nums_list.append(random.sample(range(0, 200), 10 + i * 10))
 sol = Solution()
 for i in range(N):
-    tic = time.time()
     print(sol.solve(nums_list[i],k[i]))
-    toc = time.time()
-    elapsed.append(toc-tic)
-    print(toc - tic)
